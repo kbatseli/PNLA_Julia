eb5526d3ad5284c7aafd48eac88403c315add7f8
coronasky <coronasky@gmail.com>
2013-12-06 14:49:31 +0800

    added sparseN

--- src/PNLA.jl
+++ src/PNLA.jl
@@ -1,6 +1,6 @@
 module PNLA
 
-export getD0, getDorig, getMDim, feti, fite, getM, getDreg
+export getD0, getDorig, getMDim, feti, fite, getM, getDreg, sparseN
 
 include("basics.jl")
 include("Macaulay.jl")
